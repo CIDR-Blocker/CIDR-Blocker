38d93c4a210846e5b76f87f123e05b8267aa0c03
RumbleFrog <xxlosttechxx@gmail.com>
2017-10-10 19:09:24 -0400

    Auto Fetch CSV

--- parser/index.js
+++ parser/index.js
@@ -3,17 +3,21 @@ const squel = require('squel');
 const request = require('request');
 const fs = require('fs');
 
-new Parser.IPCat('storage/datacenters.csv').parse((err, obj) => {
-  let IPCat = obj.toArray();
+request('https://raw.githubusercontent.com/client9/ipcat/master/datacenters.csv')
+  .pipe(fs.createWriteStream('storage/datacenters.csv'))
+  .on('close', () => {
+    new Parser.IPCat('storage/datacenters.csv').parse((err, obj) => {
+      let IPCat = obj.toArray();
 
-  let SQL = squel
-    .insert()
-    .into('cidr_list')
-    .setFieldsRows(IPCat)
-    .toString();
+      let SQL = squel
+        .insert()
+        .into('cidr_list')
+        .setFieldsRows(IPCat)
+        .toString();
 
-  let Plain = IPCat.map(block => block.cidr);
+      let Plain = IPCat.map(block => block.cidr);
 
-  fs.writeFileSync('storage/parsed.sql', SQL);
-  fs.writeFileSync('storage/parsed.json', JSON.stringify(Plain));
-});
+      fs.writeFileSync('storage/parsed.sql', SQL);
+      fs.writeFileSync('storage/parsed.json', JSON.stringify(Plain));
+    });
+  })
